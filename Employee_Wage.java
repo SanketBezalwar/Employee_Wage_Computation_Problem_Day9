960422196ac69d552924f0bf7b36954b96219f26
SanketBezalwar <sanketbezalwar45@gmail.com>
2022-10-10 14:03:05 +0530

    Added part time employee

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -6,6 +6,7 @@ public class Employee_Wage {
 	static int isPresent;
 	static int wage_Per_Hour = 20;
 	static int full_Day_Hour = 8;
+	static int part_Time_Hour = 4;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
@@ -34,11 +35,17 @@ public class Employee_Wage {
 	// calculate employee wage
 	public void dailyEmployeeWage() {
 		int dailyWage1;
+		int dailyWage2;
 
 		dailyWage1 = full_Day_Hour * wage_Per_Hour;
 		System.out.println("Calculate Daily Employee Wage : ");
 		System.out.println("Employee is Full Time");
 		System.out.println("Daily wage of Employee is " + dailyWage1 + " Rs ");
+		System.out.println();
+
+		dailyWage2 = part_Time_Hour * wage_Per_Hour;
+		System.out.println("Employee is Part Time");
+		System.out.println("Daily wage of Employee is " + dailyWage2 + " Rs ");
 		System.out.println();
 	}
 }
