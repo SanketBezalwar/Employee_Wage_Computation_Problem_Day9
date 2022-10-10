6902665afece71a778b889a7f1ab94c0af064731
SanketBezalwar <sanketbezalwar45@gmail.com>
2022-10-10 13:48:21 +0530

    Display Message

---
+++ Employee_Wage.java
@@ -0,0 +1,11 @@
+package Employee_Wage_Computation;
+
+public class Employee_Wage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation");
+		System.out.println();
+
+	}
+
+}
